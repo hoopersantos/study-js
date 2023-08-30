e4b8705026562d516ff2b2633e7d94271528188d
Hooper Santos <hoopersantos@gmail.com>
2023-08-30 13:01:04 -0300

    first instructions for create and add files to git

---
+++ testegit.js
@@ -0,0 +1,15 @@
+console.log("Hello  Git!")
+
+/* 
+git status
+// verifica status da minha pasta 
+
+git add nomeDoArquivo
+// adiciona arquivo a arvore do git
+
+git commit -m "comentario"
+// add comentario para o ultimo arquivo
+
+git push
+// envia commits para o github
+*/
