19c1e864c09a657ce57dd693ddd0b904a69393e4
Hooper Santos <hoopersantos@gmail.com>
2023-09-08 17:46:54 -0300

    my first site with bootstrap

---
+++ fundamentos-js/erro.js
@@ -0,0 +1,24 @@
+function tratarErroELancar(erro) {
+    //throw new Error('nao funfou')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        msg: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUppercCase() + '!!!')
+    } catch(e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('final')
+    }
+}
+
+const obj = { nome: 'Roberto' }
+imprimirNomeGritado(obj)
